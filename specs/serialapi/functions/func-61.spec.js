f63bcb92267b08806fa7d1a16d88fdbfd4f4c740
Eric Poitras <eric.poitras@gmail.com>
2019-10-22 16:29:18 -0400

    feat(ci): Added tests.

---
+++ specs/serialapi/functions/func-61.spec.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-expressions */
+const { standardEncodeRequestSpecs, standardDecodeResponseSpecs } = require('../../tools/test-frame-codec')
+const sut = require('../../../lib/serialapi/functions/func-61')
+
+standardEncodeRequestSpecs(sut, {
+  success: {
+    1: {
+      request: { nodeId: 0x23 },
+      callbackId: 0x01,
+      expected: [0x23, 0x01]
+    }
+  }
+})
+
+standardDecodeResponseSpecs(sut, {
+  success: {
+    1: {
+      data: '01',
+      expected: {
+        success: true
+      }
+    },
+    2: {
+      data: '00',
+      expected: {
+        success: false
+      }
+    }
+  }
+
+})
---
+++ specs/serialapi/functions/func-62.spec.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-expressions */
+const { standardDecodeResponseSpecs } = require('../../tools/test-frame-codec')
+const sut = require('../../../lib/serialapi/functions/func-62')
+
+standardDecodeResponseSpecs(sut, {
+  success: {
+    1: {
+      data: '01',
+      expected: {
+        success: true
+      }
+    },
+    2: {
+      data: '00',
+      expected: {
+        success: false
+      }
+    }
+  }
+
+})
